11fcda28b33845ccd61e3166a9fae6d16aae2ba9
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-05-02 10:33:14 -0500

    get profile function definition

--- app/utils/api.js
+++ app/utils/api.js
@@ -4,6 +4,13 @@ var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
 var params = "?client_id=" + id + "&client_secret=" + sec;
 
+function getProfile(username) {
+	return axios.get('https://api.github.com/users/' + username + params)
+		.then(function(user) {
+			return user.data;
+		}); 
+}
+
 module.exports = {
 
 	battle: function(players) {
