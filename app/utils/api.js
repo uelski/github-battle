e575a397fc5ade4846bfd38bd0c4bc4523b88e20
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-05-02 10:25:10 -0500

    starting api calls for results component

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,12 +1,21 @@
 var axios = require('axios');
 
-module.exports = {
-  fetchPopularRepos: function (language) {
-    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
 
-    return axios.get(encodedURI)
-      .then(function (response) {
-        return response.data.items;
-      });
-  }
+module.exports = {
+
+	battle: function(players) {
+
+	},
+
+  	fetchPopularRepos: function (language) {
+    	var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+    	return axios.get(encodedURI)
+      		.then(function (response) {
+        	return response.data.items;
+      	});
+  	}
 };
