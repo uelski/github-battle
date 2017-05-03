1f15452b6971a5ab83c8677a40ef7bb0fea9962d
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-05-03 12:04:20 -0500

    initial use of Loading component in Popular

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,6 +1,38 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
-class Loading extends React.component {
-	
+var styles = {
+	content: {
+		textAlign: 'center',
+		fontSize: '35px'
+	}
 }
+
+class Loading extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			text: props.text
+		}
+
+	}
+
+	render () {
+		return (
+			<p style={styles.content}>
+				{this.state.text}
+			</p>	
+		)
+	}
+}
+
+Loading.propTypes = {
+	text: PropTypes.string.isRequired
+}
+
+Loading.defaultProps = {
+	text: 'Loading'
+}
+
+module.exports = Loading;
