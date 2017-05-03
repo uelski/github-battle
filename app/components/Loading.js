58fa0d0531b20facb33c36c1f3cfe2812d48d324
Sam Vredenburgh <sam.vburgh@gmail.com>
2017-05-03 12:25:14 -0500

    reusable Loading component complete

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -18,6 +18,29 @@ class Loading extends React.Component {
 
 	}
 
+	componentDidMount () {
+		var stopper = this.props.text + '...';
+		this.interval = window.setInterval(function() {
+			if (this.state.text === stopper) {
+				this.setState(function() {
+					return {
+						text: this.props.text
+					}
+				})
+			} else {
+				this.setState(function(prevState) {
+					return {
+						text: prevState.text + '.'
+					}
+				})
+			}
+		}.bind(this), this.props.speed);
+	}
+
+	componentWillUnmount () {
+		window.clearInterval(this.interval);
+	}
+
 	render () {
 		return (
 			<p style={styles.content}>
@@ -28,11 +51,13 @@ class Loading extends React.Component {
 }
 
 Loading.propTypes = {
-	text: PropTypes.string.isRequired
+	text: PropTypes.string.isRequired,
+	speed: PropTypes.number.isRequired
 }
 
 Loading.defaultProps = {
-	text: 'Loading'
+	text: 'Loading',
+	speed: 300
 }
 
 module.exports = Loading;
